192005e578b3303acd33d82fa28d9f4ebe410fcf
Jonathan Wang <j2279wan@edu.uwaterloo.ca>
2019-03-10 13:17:47 -0400

    Initial commit

---
+++ content.js
@@ -0,0 +1,274 @@
+var goosePicture = "https://upload.wikimedia.org/wikipedia/commons/e/e0/Canada_goose_head_detail.JPG";
+var spanPicture = 'url("https://upload.wikimedia.org/wikipedia/commons/e/e0/Canada_goose_head_detail.JPG")';
+
+var animals = [
+  "Aardvark",
+  "Albatross",
+  "Alligator",
+  "Alpaca",
+  "Ant",
+  "Ape",
+  "Armadillo",
+  "Donkey",
+  "Baboon",
+  "Badger",
+  "Barracuda",
+  "Bat",
+  "Bear",
+  "Beaver",
+  "Bee",
+  "Bison",
+  "Boar",
+  "Buffalo",
+  "Butterfly",
+  "Camel",
+  "Capybara",
+  "Caribou",
+  "Cassowary",
+  "Cat",
+  "Caterpillar",
+  "Cattle",
+  "Chamois",
+  "Cheetah",
+  "Chicken",
+  "Chimpanzee",
+  "Chinchilla",
+  "Cobra",
+  "Cockroach",
+  "Cod",
+  "Cormorant",
+  "Coyote",
+  "Crab",
+  "Crane",
+  "Crocodile",
+  "Crow",
+  "Curlew",
+  "Deer",
+  "Dinosaur",
+  "Dog",
+  "Dogfish",
+  "Dolphin",
+  "Dotterel",
+  "Dove",
+  "Dragonfly",
+  "Duck",
+  "Dugong",
+  "Dunlin",
+  "Eagle",
+  "Echidna",
+  "Eel",
+  "Eland",
+  "Elephant",
+  "Elk",
+  "Emu",
+  "Falcon",
+  "Ferret",
+  "Finch",
+  "Fish",
+  "Flamingo",
+  "Fly",
+  "Fox",
+  "Frog",
+  "Gaur",
+  "Gazelle",
+  "Gerbil",
+  "Giraffe",
+  "Gnat",
+  "Gnu",
+  "Goat",
+  "Goldfinch",
+  "Goldfish",
+  "Goose",
+  "Gorilla",
+  "Goshawk",
+  "Grasshopper",
+  "Grouse",
+  "Guanaco",
+  "Gull",
+  "Hamster",
+  "Hare",
+  "Hawk",
+  "Hedgehog",
+  "Heron",
+  "Herring",
+  "Hippopotamus",
+  "Hornet",
+  "Horse",
+  "Human",
+  "Hummingbird",
+  "Hyena",
+  "Ibex",
+  "Ibis",
+  "Jackal",
+  "Jaguar",
+  "Jay",
+  "Jellyfish",
+  "Kangaroo",
+  "Kingfisher",
+  "Koala",
+  "Kookabura",
+  "Kouprey",
+  "Kudu",
+  "Lapwing",
+  "Lark",
+  "Lemur",
+  "Leopard",
+  "Lion",
+  "Llama",
+  "Lobster",
+  "Locust",
+  "Loris",
+  "Louse",
+  "Lyrebird",
+  "Magpie",
+  "Mallard",
+  "Manatee",
+  "Mandrill",
+  "Mantis",
+  "Marten",
+  "Meerkat",
+  "Mink",
+  "Mole",
+  "Mongoose",
+  "Monkey",
+  "Moose",
+  "Mosquito",
+  "Mouse",
+  "Mule",
+  "Narwhal",
+  "Newt",
+  "Nightingale",
+  "Octopus",
+  "Okapi",
+  "Opossum",
+  "Oryx",
+  "Ostrich",
+  "Otter",
+  "Owl",
+  "Oyster",
+  "Panther",
+  "Parrot",
+  "Partridge",
+  "Peafowl",
+  "Pelican",
+  "Penguin",
+  "Pheasant",
+  "Pig",
+  "Pigeon",
+  "Pony",
+  "Porcupine",
+  "Porpoise",
+  "Quail",
+  "Quelea",
+  "Quetzal",
+  "Rabbit",
+  "Raccoon",
+  "Rail",
+  "Ram",
+  "Rat",
+  "Raven",
+  "Red deer",
+  "Red panda",
+  "Reindeer",
+  "Rhinoceros",
+  "Rook",
+  "Salamander",
+  "Salmon",
+  "Sand Dollar",
+  "Sandpiper",
+  "Sardine",
+  "Scorpion",
+  "Seahorse",
+  "Seal",
+  "Shark",
+  "Sheep",
+  "Shrew",
+  "Skunk",
+  "Snail",
+  "Snake",
+  "Sparrow",
+  "Spider",
+  "Spoonbill",
+  "Squid",
+  "Squirrel",
+  "Starling",
+  "Stingray",
+  "Stinkbug",
+  "Stork",
+  "Swallow",
+  "Swan",
+  "Tapir",
+  "Tarsier",
+  "Termite",
+  "Tiger",
+  "Toad",
+  "Trout",
+  "Turkey",
+  "Turtle",
+  "Viper",
+  "Vulture",
+  "Wallaby",
+  "Walrus",
+  "Wasp",
+  "Weasel",
+  "Whale",
+  "Wildcat",
+  "Wolf",
+  "Wolverine",
+  "Wombat",
+  "Woodcock",
+  "Woodpecker",
+  "Worm",
+  "Wren",
+  "Yak",
+  "Zebra"
+]
+
+chrome.runtime.onMessage.addListener(
+  function (request) {
+    if (request.message === "start") {
+      start();
+    }
+  }
+);
+
+function start() {
+  var images = document.getElementsByTagName("img");
+
+  for (var i = 0; i < images.length; ++i) {
+    images[i].src = goosePicture;
+    images[i].srcset = goosePicture;
+  }
+
+  console.log("hello")
+
+  var elements = document.getElementsByTagName('*');
+
+  console.log("helo2")
+
+  for (var x = 0; x < animals.length; x++) {
+    var replacer = new RegExp(animals[x], "gi");
+
+    console.log(replacer);
+
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+          var text = node.nodeValue;
+
+          var replacedText = text.replace(replacer, 'goose');
+
+
+          if (replacedText !== text) {
+            element.replaceChild(document.createTextNode(replacedText), node);
+          }
+        }
+      }
+    }
+  }
+
+}
